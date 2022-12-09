ba3555d04aca72ad43eb18b53231a80ff4f07f6d
Jason Kundi <jasonkundi1996@gmail.com>
2022-12-09 18:56:33 +0000

    working on calculator logic

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+const Operations = {
+    "+": (numOne, numTwo) => numOne + numTwo,
+    "-": (numOne, numTwo) => numOne - numTwo,
+    "*": (numOne, numTwo) => numOne * numTwo,
+    "/": (numOne, numTwo) => numOne / numTwo,
+  }
+
+  
+
